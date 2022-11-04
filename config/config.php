2cdf2a63fbc8c1e760accf20a103140905ce364f
Thomas Moraes <tho.vini7@gmail.com>
2022-11-04 09:42:05 -0300

    Clean Architecture - Repository Mongodb

--- config/config.php
+++ config/config.php
@@ -2,3 +2,5 @@
 
 // Display Errors
 // ini_set('display_errors', 1);
+
+define("DB_DATABASE", 'alura_students');
--- src/Infraestructure/Student/StudentRepositoryMongo.php
+++ src/Infraestructure/Student/StudentRepositoryMongo.php
@@ -1,13 +1,17 @@
 <?php 
 namespace CleanArchitectureApp\Infraestructure\Student;
 
-use CleanArchitectureApp\Domain\Cpf;
-use CleanArchitectureApp\Domain\Student\Student;
 use MongoDB\Client;
+use CleanArchitectureApp\Domain\Cpf;
+use MongoDB\Operation\FindOneAndUpdate;
+use CleanArchitectureApp\Domain\Student\Student;
 use CleanArchitectureApp\Domain\Student\StudentRepository;
 
 class StudentRepositoryMongo implements StudentRepository
 {
+    /**
+     * @var MongoDB\Client
+     */
     private $client;
 
     private $collection = "students";
@@ -15,20 +19,45 @@ class StudentRepositoryMongo implements StudentRepository
     public function __construct(Client $client)
     {
         $this->client = $client;
+        $this->collection = $this->client->selectCollection(DB_DATABASE, $this->collection);
     }
 
     public function addStudent(Student $student): void
     {
-        
+        $id = $this->nextId();
+
+        $document = [
+            '_id' => $id,
+            'name' => $student->getName(),
+            'email' => (string) $student->getEmail(),
+            'cpf' => (string) $student->getCpf(),
+            'phones' => $student->phones()
+        ];
+
+        $this->client->insertOne($document);
     }
 
     public function findByCpf(Cpf $cpf): Student
     {
-        
+        $find = $this->client->find(['cpf' => (string) $cpf]);
+        return Student::create($find['cpf'], $find['name'], $find['email']);
     }
 
     public function findAllStudents(): array
     {
-        
+        return $this->client->find();
+    }
+
+    private function nextId()
+    {
+        $collection = $this->client->selectCollection("counters");
+
+        $result = $collection->findOneAndUpdate(
+            ['_id' => 'student_id'],
+            ['$inc' => ['seq' => 1]],
+            ['upsert' => true, 'projection' => [ 'seq' => 1 ],'returnDocument' => FindOneAndUpdate::RETURN_DOCUMENT_AFTER]
+        );
+
+        return $result['seq'];
     }
 }
